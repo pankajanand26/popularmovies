f8a49eab3b4fde57ab4597443b9245532ce459f3
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-08-22 14:57:29 +0530

    First Movies project commit

---
+++ app/src/main/java/com/example/android/popularmovies/PopularMoviesFragment.java
@@ -0,0 +1,41 @@
+package com.example.android.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PopularMoviesFragment extends Fragment {
+
+    public PopularMoviesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootview=inflater.inflate(R.layout.fragment_movies, container, false);;
+
+
+        GridView gridview = (GridView) rootview.findViewById(R.id.moviesgrid);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootview;
+
+        }
+}
